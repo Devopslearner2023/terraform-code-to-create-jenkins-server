7ded3589de44322cc3b029aa487a9cbb06b630dc
Gopakumar <132288346+Devopslearner2023@users.noreply.github.com>
2024-04-20 23:52:42 +0530

    Update jenkins-server.tf

--- jenkins-server.tf
+++ jenkins-server.tf
@@ -1,26 +1,24 @@
-data "aws_ami" "latest-amazon-linux-image" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
+
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "myapp-server" {
-  ami                         = data.aws_ami.latest-amazon-linux-image.id
-  instance_type               = var.instance_type
-  key_name                    = "jenkins-server"
-  subnet_id                   = aws_subnet.myapp-subnet-1.id
-  vpc_security_group_ids      = [aws_default_security_group.default-sg.id]
-  availability_zone           = var.avail_zone
-  associate_public_ip_address = true
-  user_data                   = file("jenkins-server-script.sh")
+resource "aws_instance" "jenkins-1" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.medium"
+
   tags = {
-    Name = "${var.env_prefix}-server"
+    Name = "HelloWorld"
   }
 }
